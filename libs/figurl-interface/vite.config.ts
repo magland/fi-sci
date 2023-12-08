45a70c356b0de2640301c93a4b511c0e0338c0de
Jeremy Magland <jmagland@flatironinstitute.org>
2023-12-08 08:01:30 -0500

    publish figurl-interface

--- libs/figurl-interface/vite.config.ts
+++ libs/figurl-interface/vite.config.ts
@@ -37,7 +37,12 @@ export default defineConfig({
     },
     rollupOptions: {
       // External packages that should not be bundled into your library.
-      external: ['react', 'react-dom', 'react/jsx-runtime'],
+      // everythin of the form node_modules/*
+      external: [
+        'react', 'react-dom', 'react/jsx-runtime',
+        // exclude everything that does not start with "/"
+        /^(?!\/).*/
+      ],
     },
   },
 });
