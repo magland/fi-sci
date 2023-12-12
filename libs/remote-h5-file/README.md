a3e0f3aa83d81bbefdbd58199ac2cb80c7fe2750
Jeremy Magland <jmagland@flatironinstitute.org>
2023-12-12 13:01:32 -0500

    add remote-h5-file

---
+++ libs/remote-h5-file/README.md
@@ -0,0 +1,7 @@
+# remote-h5-file
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test remote-h5-file` to execute the unit tests via [Vitest](https://vitest.dev/).
---
+++ libs/remote-h5-file/src/index.ts
@@ -0,0 +1,2 @@
+export { RemoteH5File, MergedRemoteH5File, getRemoteH5File, getMergedRemoteH5File, globalRemoteH5FileStats } from './lib/RemoteH5File'
+export type { RemoteH5FileX, RemoteH5Dataset, RemoteH5Group, RemoteH5Subdataset, RemoteH5Subgroup } from './lib/RemoteH5File'
