9bdc967ddde76ca6bf63e8616f009602f05961d4
Jeremy Magland <jmagland@flatironinstitute.org>
2024-02-28 09:41:30 +0000

    brainhack-ophys-dev: load vis_traces.json

--- apps/brainhack-ophys-dev/src/app/App.tsx
+++ apps/brainhack-ophys-dev/src/app/App.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useEffect, useState } from "react";
 import './App.css'
 import PlaneSegmentationView from "./PlaneSegmentationView";
 import ExamplePlotlyComponent from "./ExamplePlotlyComponent";
@@ -9,6 +9,23 @@ type Props = {
 
 const App: FunctionComponent<Props> = () => {
   const a: number = 4
+
+  const [visTracesData, setVisTracesData] = useState<any>(undefined)
+
+  useEffect(() => {
+    ;(async () => {
+      const response = await fetch('https://neurosift.org/tmp/vis_traces.json')
+      const data = await response.json()
+      setVisTracesData(data)
+    })()
+  }, [])
+
+  console.info('visTracesData', visTracesData)
+
+  if (!visTracesData) {
+    return <div>Loading vis_traces.json</div>
+  }
+
   return (
     <div>
       <h1>Brainhack Ophys Dev {a}</h1>
