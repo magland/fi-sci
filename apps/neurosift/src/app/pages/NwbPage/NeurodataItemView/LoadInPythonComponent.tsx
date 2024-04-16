00669e5dec6f03567ca14d4eb0c0092a6971e7eb
Jeremy Magland <jmagland@flatironinstitute.org>
2024-04-16 11:12:24 -0400

    neurosift: fix "load in python" for lindi

--- apps/neurosift/src/app/pages/NwbPage/NeurodataItemView/LoadInPythonComponent.tsx
+++ apps/neurosift/src/app/pages/NwbPage/NeurodataItemView/LoadInPythonComponent.tsx
@@ -1,7 +1,7 @@
 import { FunctionComponent, useMemo } from "react"
 import { useModalDialog } from "../../../ApplicationBar"
 import { useNwbFile } from "../NwbFileContext"
-import { MergedRemoteH5File, RemoteH5File, RemoteH5Group } from "@fi-sci/remote-h5-file"
+import { MergedRemoteH5File, RemoteH5File, RemoteH5FileLindi, RemoteH5Group } from "@fi-sci/remote-h5-file"
 import { findViewPluginsForType, ViewPlugin } from "../viewPlugins/viewPlugins"
 import { Hyperlink } from "@fi-sci/misc"
 import ModalWindow from "@fi-sci/modal-window"
@@ -37,23 +37,37 @@ const LoadInPythonWindow: FunctionComponent<Props> = ({path, group, viewName}) =
     const nwbFile = useNwbFile()
 
     let nwbFileUrl: string
+    let urlType: 'hdf5' | 'lindi'
     if (nwbFile instanceof MergedRemoteH5File) {
         const f = nwbFile.getFiles()[0]
-        if (f instanceof RemoteH5File) {
+        if (f instanceof RemoteH5FileLindi) {
             nwbFileUrl = f.url
+            urlType = 'lindi'
+        }
+        else if (f instanceof RemoteH5File) {
+            nwbFileUrl = f.url
+            urlType = 'hdf5'
         }
         else {
             nwbFileUrl = 'unknown'
+            urlType = 'hdf5'
         }
     }
     else {
-        nwbFileUrl = nwbFile.url
+        if (nwbFile instanceof RemoteH5FileLindi) {
+            nwbFileUrl = nwbFile.url
+            urlType = 'lindi'
+        }
+        else {
+            nwbFileUrl = nwbFile.url
+            urlType = 'hdf5'
+        }
     }
 
     const source = useMemo(() => {
         const viewPlugin = findViewPluginsForType(group.attrs.neurodata_type, {nwbFile}).defaultViewPlugin
-        return createSource(nwbFileUrl, viewPlugin, path, group, viewName)
-    }, [path, group, viewName, nwbFileUrl, nwbFile])
+        return createSource(nwbFileUrl, urlType, viewPlugin, path, group, viewName)
+    }, [path, group, viewName, nwbFileUrl, urlType, nwbFile])
     return (
         <Markdown
             source={source}
@@ -61,12 +75,13 @@ const LoadInPythonWindow: FunctionComponent<Props> = ({path, group, viewName}) =
     )
 }
 
-const createSource = (url: string, viewPlugin: ViewPlugin | undefined, path: string, group: RemoteH5Group, viewName: string) => {
+const createSource = (url: string, urlType: 'hdf5' | 'lindi', viewPlugin: ViewPlugin | undefined, path: string, group: RemoteH5Group, viewName: string) => {
     const backtics = '```'
     const backtic = '`'
     // const nt = group.attrs.neurodata_type
     const customCode = viewPlugin && viewPlugin.getCustomPythonCode ? viewPlugin.getCustomPythonCode(group) : ''
-    return `
+    if (urlType === 'hdf5') {
+        return `
 ## Loading ${backtic}${path}${backtic} (${backtic}${group.attrs.neurodata_type}${backtic}).
 
 ${backtics}bash
@@ -93,6 +108,38 @@ ${backtics}
 See [remfile](https://github.com/magland/remfile) for more information on loading remote files.
 
 `
+    }
+    else if (urlType === 'lindi') {
+        return `
+## Loading ${backtic}${path}${backtic} (${backtic}${group.attrs.neurodata_type}${backtic}).
+
+${backtics}bash
+# Prerequisites:
+pip install h5py lindi
+${backtics}
+
+${backtics}python
+import h5py
+import lindi
+
+url = '${url}'
+
+# Load the remote file
+f = lindi.LindiH5pyFile.from_reference_file_system(url)
+
+# load the neurodata object
+X = f['${path}']
+${customCode}
+${backtics}
+
+## Notes
+
+See [lindi](https://github.com/neurodatawithoutborders/lindi) for more information on LINDI files.
+`
+    }
+    else {
+        return `Unexpected urlType: ${urlType}`
+    }
 }
 
 export default LoadInPythonComponent
