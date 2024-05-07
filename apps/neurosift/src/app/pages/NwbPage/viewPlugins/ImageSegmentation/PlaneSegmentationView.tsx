229d67600de540a29b145d01d723a53df53ac67a
Jeremy Magland <jmagland@flatironinstitute.org>
2024-05-07 15:45:13 -0400

    Add support for pixel masks in PlaneSegmentationView

--- apps/neurosift/src/app/pages/NwbPage/viewPlugins/ImageSegmentation/PlaneSegmentationView.tsx
+++ apps/neurosift/src/app/pages/NwbPage/viewPlugins/ImageSegmentation/PlaneSegmentationView.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { RemoteH5Dataset, RemoteH5FileX, RemoteH5Group } from "@fi-sci/remote-h5-file"
 import { FunctionComponent, useEffect, useState } from "react"
@@ -22,21 +23,48 @@ type UnitMask = {
 const blockSize = 50
 class PlaneSegmentationClient {
     #imageMaskDataset: RemoteH5Dataset | undefined
+    #pixelMaskDataset: RemoteH5Dataset | undefined
+    #pixelMaskIndex: number[] | undefined
+    #pixelMaskImageSize: number[] | undefined // in the case of pixel mask, we unfortunately do not have any direct way to get the image size
     #blocks: {[i: number]: UnitMask[]} = {}
     constructor(private nwbFile: RemoteH5FileX, private objectPath: string) {
     }
     async initialize() {
         this.#imageMaskDataset = await this.nwbFile.getDataset(`${this.objectPath}/image_mask`)
-        if (!this.#imageMaskDataset) throw Error(`Unable to load image mask dataset`)
+        this.#pixelMaskDataset = await this.nwbFile.getDataset(`${this.objectPath}/pixel_mask`)
+        if (this.#pixelMaskDataset) {
+            this.#pixelMaskIndex = (await this.nwbFile.getDatasetData(`${this.objectPath}/pixel_mask_index`, {})) as any
+            this.#pixelMaskImageSize = await determineImageSizeFromNwbFileContext(this.nwbFile, this.objectPath)
+        }
+        if ((!this.#imageMaskDataset) && (!this.#pixelMaskDataset)) throw Error(`No image mask or pixel mask dataset`)
     }
     get shape() {
-        return this.#imageMaskDataset!.shape
+        if (this.#imageMaskDataset) return this.#imageMaskDataset.shape
+        else if (this.#pixelMaskDataset) {
+            if (!this.#pixelMaskImageSize) throw Error(`No pixel mask image size`)
+            return [this.#pixelMaskIndex!.length, this.#pixelMaskImageSize[0], this.#pixelMaskImageSize[1]] // see comment above
+        }
+        else throw Error(`No image mask or pixel mask dataset`)
+    }
+    get hasImageMask() {
+        return !!this.#imageMaskDataset
+    }
+    get hasPixelMask() {
+        return !!this.#pixelMaskDataset
     }
     async getImageMask(index: number) {
+        if (!this.#imageMaskDataset) throw Error(`Cannot get image mask without image mask dataset`)
         const block = await this._loadBlock(Math.floor(index / blockSize))
         return block[index % blockSize]
     }
+    async getPixelMask(index: number) {
+        const i1 = index > 0 ? this.#pixelMaskIndex![index - 1] : 0
+        const i2 = this.#pixelMaskIndex![index]
+        const data = await this.nwbFile.getDatasetData(`${this.objectPath}/pixel_mask`, {slice: [[i1, i2]]})
+        return data
+    }
     private async _loadBlock(chunkIndex: number) {
+        if (!this.#imageMaskDataset) throw Error(`Cannot load block without image mask dataset`)
         if (this.#blocks[chunkIndex]) return this.#blocks[chunkIndex]
         const i1 = chunkIndex * blockSize
         const i2 = Math.min(this.shape[0], i1 + blockSize)
@@ -70,10 +98,43 @@ class PlaneSegmentationClient {
                 data: data0
             })
         }
-        
+
         this.#blocks[chunkIndex] = block
         return block
     }
+}
+
+const determineImageSizeFromNwbFileContext = async (nwbFile: RemoteH5FileX, objectPath: string) => {
+    /*
+    In the case of pixel mask, we unfortunately do not have any direct way to get the image size.
+    We need to determine it from the context. In other words, the sibling objects.
+    */
+    let parentPath = objectPath.split('/').slice(0, -1).join('/')
+    let parentGroup = await nwbFile.getGroup(parentPath)
+    if (!parentGroup) throw Error(`Unable to get parent group for determining image size`)
+    if (parentGroup.attrs['neurodata_type'] === 'ImageSegmentation') {
+        // need to go up one more level
+        parentPath = parentPath.split('/').slice(0, -1).join('/')
+        parentGroup = await nwbFile.getGroup(parentPath)
+        if (!parentGroup) throw Error(`Unable to get parent group for determining image size`)
+    }
+    for (const sg of parentGroup.subgroups) {
+        if (sg.attrs['neurodata_type'] === 'Images') {
+            const imagesGroup = await nwbFile.getGroup(`${parentPath}/${sg.name}`)
+            if (!imagesGroup) {
+                continue
+            }
+            for (const ds of imagesGroup.datasets) {
+                if (ds.attrs['neurodata_type'] === 'Image') {
+                    if (ds.shape.length === 2) {
+                        return ds.shape
+                    }
+                }
+            }
+
+        }
+    }
+    return undefined
 }
 
 const PlaneSegmentationView: FunctionComponent<Props> = ({width, height, imageSegmentationGroup, nwbFile, selectedSegmentationName}) => {
@@ -148,28 +209,56 @@ const Test1: FunctionComponent<{client: PlaneSegmentationClient, width: number, 
                     timer = Date.now()
                 }
                 const color = distinctColors[j % distinctColors.length]
-                const aa = await client.getImageMask(j)
-                if (canceled) return
-                const {x0, y0, w0, h0, data} = aa
-                const maxval = computeMaxVal(data)
-                const imageData = ctx.createImageData(w0, h0)
-                for (let i = 0; i < w0; i++) {
-                    for (let j = 0; j < h0; j++) {
-                        const v = data[i][j] / (maxval || 1)
-                        const index = (j * w0 + i) * 4
-                        imageData.data[index + 0] = color[0] * v
-                        imageData.data[index + 1] = color[1] * v
-                        imageData.data[index + 2] = color[2] * v
+                if (client.hasImageMask) {
+                    const aa = await client.getImageMask(j)
+                    if (canceled) return
+                    const {x0, y0, w0, h0, data} = aa
+                    const maxval = computeMaxVal(data)
+                    const imageData = ctx.createImageData(N1, N2)
+                    for (let i = 0; i < w0; i++) {
+                        for (let j = 0; j < h0; j++) {
+                            const v = data[i][j] / (maxval || 1)
+                            const index = (j * w0 + i) * 4
+                            imageData.data[index + 0] = color[0] * v
+                            imageData.data[index + 1] = color[1] * v
+                            imageData.data[index + 2] = color[2] * v
+                            imageData.data[index + 3] = v ? (v * 255) : 0
+                        }
+                    }
+                    const offscreenCanvas = document.createElement('canvas')
+                    offscreenCanvas.width = w0
+                    offscreenCanvas.height = h0
+                    const c = offscreenCanvas.getContext('2d')
+                    if (!c) return
+                    c.putImageData(imageData, 0, 0)
+                    ctx.drawImage(offscreenCanvas, x0 * scale, y0 * scale, w0 * scale, h0 * scale)
+                }
+                else if (client.hasPixelMask) {
+                    const aa = await client.getPixelMask(j)
+                    if (!aa) throw Error(`Unable to load pixel mask data`)
+                    const values: {x: number, y: number, v: number}[] = []
+                    for (let i = 0; i < aa.length; i++) {
+                        const b = aa[i] as any
+                        values.push({x: b[0], y: b[1], v: b[2]})
+                    }
+                    const maxval = values.reduce((maxval, v) => Math.max(maxval, v.v), 0)
+                    const imageData = ctx.createImageData(N1, N2)
+                    for (let i = 0; i < values.length; i++) {
+                        const {x, y, v} = values[i]
+                        const index = (y * N1 + x) * 4
+                        imageData.data[index + 0] = color[0] * v / maxval
+                        imageData.data[index + 1] = color[1] * v / maxval
+                        imageData.data[index + 2] = color[2] * v / maxval
                         imageData.data[index + 3] = v ? (v * 255) : 0
                     }
+                    const offscreenCanvas = document.createElement('canvas')
+                    offscreenCanvas.width = N1
+                    offscreenCanvas.height = N2
+                    const c = offscreenCanvas.getContext('2d')
+                    if (!c) return
+                    c.putImageData(imageData, 0, 0)
+                    ctx.drawImage(offscreenCanvas, 0, 0, N1 * scale, N2 * scale)
                 }
-                const offscreenCanvas = document.createElement('canvas')
-                offscreenCanvas.width = w0
-                offscreenCanvas.height = h0
-                const c = offscreenCanvas.getContext('2d')
-                if (!c) return
-                c.putImageData(imageData, 0, 0)
-                ctx.drawImage(offscreenCanvas, x0 * scale, y0 * scale, w0 * scale, h0 * scale)
             }
             setLoadingMessage(`Loaded ${N0} units`)
         }
